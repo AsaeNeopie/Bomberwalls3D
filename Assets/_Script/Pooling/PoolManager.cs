9e37709c9dfb3df796172e7dac8da90bf52306b0
nathan-exe <nathan.tazi.1@gmail.com>
2025-01-06 14:52:09 +0100

    etsbexxyi^$:ù^m!

--- Assets/_Script/Pooling/PoolManager.cs
+++ Assets/_Script/Pooling/PoolManager.cs
@@ -30,11 +30,7 @@ public class PoolManager : MonoBehaviour
         instance = this;
     }
 
-    [SerializeField] List<Pool> _pools = new List<Pool>();
+    public Pool bombPool;
 
-    public Pool SelectPool(Pools selectedPool)
-    {
-        return _pools[(int)selectedPool];
-    }
 
 }
