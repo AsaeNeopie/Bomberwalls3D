38c7ba7d1e698c6eface7a5be1594354a0fc023f
Asae <n.noahchery@gmail.com>
2025-01-06 15:39:30 +0100

    bombe qui marche

--- Assets/_Script/BombPickup.cs
+++ Assets/_Script/BombPickup.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class BombPickup : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             this.gameObject.SetActive(false);
             Debug.Log("Bombe ramassé");
         }
-    }
+    }*/
 }
