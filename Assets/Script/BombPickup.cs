8c050b3abf80f3280ea280b7efffa0065e0140c5
Asae <n.noahchery@gmail.com>
2025-01-06 14:27:12 +0100

    BombPickup

---
+++ Assets/Script/BombPickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.gameObject.SetActive(false);
+            Debug.Log("Bombe ramassé");
+        }
+    }
+}
