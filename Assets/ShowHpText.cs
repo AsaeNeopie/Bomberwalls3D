d4d87149d8341da2c999a26f357a6acb5cda4ffe
Asae <n.noahchery@gmail.com>
2025-01-16 12:42:54 +0100

    update bomb text ui

---
+++ Assets/ShowHpText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowBombText : MonoBehaviour
+{
+    TMP_Text _bombText;
+    [SerializeField] PlayerBombing _playerBombing;
+    int _bombInInvenotory;
+    public void Start()
+    {
+        _playerBombing.OnBombPickedUp += updateText;
+        TryGetComponent(out _bombText);
+    }
+
+    public void updateText()
+    {
+        _bombInInvenotory++;
+        _bombText.text = _bombInInvenotory.ToString();
+    }
+}
